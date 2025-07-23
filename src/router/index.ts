aff95768998eee2c53beb34822f44aec7d062bc4
The Bearodactyl <dabewodactowluwu@gmail.com>
2025-07-22 21:51:59 -0500

    small fix bc the other fix didn't work :(

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import IndexView from '@/views/IndexView.vue'
 import DefaultLayout from '@/components/DefaultLayout.vue'
 import ReadListView from '@/views/ReadListView.vue'
@@ -11,7 +11,7 @@ import OnePieceView from '@/views/OnePieceView.vue'
 import TarpitView from '@/views/TarpitView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
