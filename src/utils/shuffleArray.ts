67096eddf763dc11b108a2619853a1fa98345717
The Bearodactyl <dabewodactowluwu@gmail.com>
2025-07-23 00:39:07 -0500

    better shuffle algo

--- src/utils/shuffleArray.ts
+++ src/utils/shuffleArray.ts
@@ -1,13 +1,29 @@
 export function shuffleArray<T>(input: readonly T[]): T[] {
   const array = input.slice()
 
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
+  const getRandomIndex = (max: number): number => {
+    let randomValue
 
-    if (j >= 0 && j < array.length) {
-      const temp = array[i]
-      array[i] = array[j]
-      array[j] = temp
+    if (typeof window !== "undefined") {
+      randomValue = window.crypto.getRandomValues(new Uint32Array(1))[0]
+    } else {
+      randomValue = Math.random()
+    }
+
+    return Math.floor((randomValue * max + Math.random() * max) % (max + 1))
+  }
+
+  const passes = 20
+
+  for (let k = 0; k < passes; k++) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = getRandomIndex(i)
+
+      if (j >= 0 && j < array.length) {
+        const temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+      }
     }
   }
 
