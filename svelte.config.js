4ebf7f4e7a7b5dcc3dfdb7910a0bfd87cb6c3715
The Bearodactyl <dabewodactowluwu@gmail.com>
2025-07-25 13:12:19 -0500

    why

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,17 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapterGhPages from "svelte-adapter-ghpages"
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapterGhPages({
-			pages: "build",
-			assets: "build",
-			fallback: "404.html",
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: "/bearodactyl.dev"
-		}
-	}
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: "/bearodactyl.dev/"
+    }
+  }
 };
 
 export default config;
