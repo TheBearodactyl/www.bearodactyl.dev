2d46184ff47f098a038b6d880e9089bc980e51ea
The Bearodactyl <dabewodactowluwu@gmail.com>
2025-07-30 18:18:48 -0500

    fix `WarningIcon`

--- src/components/icons/WarningIcon.svelte
+++ src/components/icons/WarningIcon.svelte
@@ -1,4 +1,4 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="32mm" height="32mm" viewBox="0 0 64 64">
+<svg xmlns="http://www.w3.org/2000/svg" width="16mm" height="16mm" viewBox="0 0 64 64">
     <g fill="none" stroke="#ff0f0f" stroke-width="3.4">
         <path
             d="M5 57h6l2 1h33l1-1h7v-2l-2-8-1-2v-1l-1-5-1-1v-2l-1-1-1-2-1-2-3-6-1-3-1-1-1-2-2-3-3-6V8c-2-2 0 2-1-1l-1-1-1 1-3 3-5 5-5 6-1 2-2 3-2 3-3 6-1 2v2l-2 3-1 3-1 3-1 4-1 2v3h1z"
