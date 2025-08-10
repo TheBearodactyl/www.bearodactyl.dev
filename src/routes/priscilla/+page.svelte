68561d821234114fc134fa5941697f5d41305a2b
The Bearodactyl <dabewodactowluwu@gmail.com>
2025-08-09 20:43:40 -0500

    fix the priscilla page again

--- src/routes/priscilla/+page.svelte
+++ src/routes/priscilla/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     import { choose_rand } from "$lib/utils/rand";
     import LinkedImage from "../../components/misc/LinkedImage.svelte";
+</script>
 
-    const priscilla_images = [
+<LinkedImage
+    imageSrc={choose_rand([
         "/images/priscilla/PXL_20250805_000809322.jpg",
         "/images/priscilla/PXL_20250806_052249930.jpg",
         "/images/priscilla/PXL_20250806_052304438.jpg",
@@ -10,11 +12,7 @@
         "/images/priscilla/PXL_20250809_135841557.jpg",
         "/images/priscilla/PXL_20250810_012524573.PORTRAIT.ORIGINAL.jpg",
         "/images/priscilla/PXL_20250810_012611064.jpg",
-    ];
-</script>
-
-<LinkedImage
-    imageSrc={choose_rand(priscilla_images)}
+    ])}
     audioSrc=""
 ></LinkedImage>
 
