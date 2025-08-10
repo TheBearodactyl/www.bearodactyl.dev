9f0195bea616ba9dd2cce94ad491ce321e862c86
The Bearodactyl <dabewodactowluwu@gmail.com>
2025-08-09 20:49:26 -0500

    lmfao

--- src/routes/priscilla/+page.svelte
+++ src/routes/priscilla/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
     import { choose_rand } from "$lib/utils/rand";
     import LinkedImage from "../../components/misc/LinkedImage.svelte";
-</script>
 
-<LinkedImage
-    imageSrc={choose_rand([
+    const images = [
         "/images/priscilla/PXL_20250805_000809322.jpg",
         "/images/priscilla/PXL_20250806_052249930.jpg",
         "/images/priscilla/PXL_20250806_052304438.jpg",
@@ -12,12 +10,16 @@
         "/images/priscilla/PXL_20250809_135841557.jpg",
         "/images/priscilla/PXL_20250810_012524573.PORTRAIT.ORIGINAL.jpg",
         "/images/priscilla/PXL_20250810_012611064.jpg",
-    ])}
-    audioSrc=""
-></LinkedImage>
+    ];
 
-<style>
-    :global {
-        @import url("/src/assets/css/main.css");
-    }
-</style>
+    let selectedImage = $state(choose_rand(images));
+
+    $effect(() => {
+        selectedImage = choose_rand(images);
+    });
+</script>
+
+<LinkedImage
+    imageSrc={selectedImage}
+    audioSrc=""
+/>
